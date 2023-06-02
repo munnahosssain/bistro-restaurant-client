93fb73edaf3b5fb830a4860e9fbf32b96938b679
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2023-06-02 14:15:37 +0600

    json web token applied

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
 import {
   GithubAuthProvider,
   GoogleAuthProvider,
@@ -58,6 +59,17 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, currentUser => {
       setUser(currentUser);
       // console.log(currentUser);
+      if (currentUser) {
+        axios
+          .post("http://localhost:5000/jwt", { email: currentUser.email })
+          .then(data => {
+            console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
+          });
+      } else {
+        localStorage.removeItem("access-token");
+      }
+
       setLoading(false);
     });
     return () => {
